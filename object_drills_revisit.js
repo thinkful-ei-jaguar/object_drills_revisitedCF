8d8199152cc96c4091f25f4040869dddcfde1824
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-03 15:30:01 -0800

    added self-reference drill

--- object_drills_revisit.js
+++ object_drills_revisit.js
@@ -22,3 +22,18 @@ function updateObject(obj) {
     obj.bang = "bang";
     return obj;
 }
+
+//Self-Reference 
+
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function() {
+          return `${this.firstName} ${this.lastName}`
+      },
+    };
+    return person;
+  }
